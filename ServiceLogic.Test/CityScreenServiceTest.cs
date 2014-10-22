e92fac1bd022523f254c2156554d0e19d1613c2c
joeiren <joeiren.wang@gmail.com>
2014-10-22 08:54:05 +0800

    add CityScreen services and function

---
+++ ServiceLogic.Test/CityScreenServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartHaiShu.CityScreenServices;
+using SmartHaiShu.CityScreenServices.SocialInsureService;
+
+
+namespace ServiceLogic.Test
+{
+    [TestClass]
+    public class CityScreenServiceTest
+    {
+        [TestMethod]
+        public void SocialInsureTest()
+        {
+            var query = new SocialInsureQuery();
+            query.Query("","");
+        }
+
+        [TestMethod]
+        public void WeatherTest()
+        {
+            var weather = new WeatherQuery();
+            var result =weather.QueryToday();
+        }
+
+        [TestMethod]
+        public void NewsTest()
+        {
+            var query = new NewsQuery();
+            var news = query.EducationNews(10,0);
+            news = query.FavorableNews(10, 0);
+            news = query.HealthNews(10, 0);
+            news = query.PriceNews(10, 0);
+            news = query.TripNews(10, 0);
+        }
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            var query = new PriceQuery();
+            query.Query();
+        }
+    }
+}
---
+++ SmartHaiShu.CityScreenServices/PriceQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SmartHaiShu.CityScreenServices.PriceService;
+using SmartHaiShu.Utility;
+
+
+namespace SmartHaiShu.CityScreenServices
+{
+    public class PriceQuery
+    {
+
+        public void Query()
+        {
+            try
+            {
+                PriceService.PriceClient client = new PriceClient();
+                var foods = client.GetFoods();
+                var categ = client.GetCategorys();
+                var tables = client.GetTableNames();
+                var dd = client.GetMonitorSites();
+                PriceService.FoodMonitor monitor = new FoodMonitor();
+                monitor.FoodID = foods[0].Key;
+                monitor.FoodName = foods[0].Value;
+                var result = client.FindFoodMonitors(monitor, null, true, 10, 0);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.GetInstance().Error(ex.ToString());
+                return ;
+            }
+            
+        }
+    }
+}
